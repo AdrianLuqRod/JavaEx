617b7e41a5c32eead8a87e297e778333f3f8c44d
Adrián <adrian.luque.rod@gmail.com>
2023-11-02 10:40:56 +0100

    updates

--- EjerciciosClase/Unidad_2/u2e29-dado/src/main/java/es/casa/PruebaDado.java
+++ EjerciciosClase/Unidad_2/u2e29-dado/src/main/java/es/casa/PruebaDado.java
@@ -5,12 +5,19 @@ import java.util.Scanner;
 public class PruebaDado {
     public static void main(String[] args) {
         boolean acierto = false;
+        int adiv;
         Scanner sc = new Scanner(System.in);
         Dado d = new Dado();
         System.out.printf("Pon el número que te gustaría que te saliese: ");
-        int adiv = sc.nextInt();
-        sc.nextLine();
         do {
+            adiv = sc.nextInt();
+            sc.nextLine();
+            if(adiv<=0 || adiv>6){
+                System.out.printf("Ese numero no puede salir, pon otro: ");
+            }
+        } while (adiv<=0 || adiv>6);
+        do {
+            
             int result = d.tirada();
             System.out.println(result);
             if (result == adiv) {
