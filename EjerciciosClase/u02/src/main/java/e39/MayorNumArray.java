5c852999ff89f18bd0f31ea6aa36380920f68a76
Adrián <adrian.luque.rod@gmail.com>
2023-11-08 10:56:16 +0100

    Update e39

--- EjerciciosClase/u02/src/main/java/e39/MayorNumArray.java
+++ EjerciciosClase/u02/src/main/java/e39/MayorNumArray.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class MayorNumArray {
     public static void main(String[] args) {
         int[] enteros = new int[500];
-        int max = 0;
+        int max = Integer.MIN_VALUE;
         Random r = new Random();
         for (int i = 0; i < enteros.length; i++) {
             enteros[i] = r.nextInt(1000001);
