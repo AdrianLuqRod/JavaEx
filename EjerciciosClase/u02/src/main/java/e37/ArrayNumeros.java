0c3d34866bdab08610737210b5c7b550ba91a284
Adrián <adrian.luque.rod@gmail.com>
2023-11-08 10:32:47 +0100

    feat:Update e37

--- EjerciciosClase/u02/src/main/java/e37/ArrayNumeros.java
+++ EjerciciosClase/u02/src/main/java/e37/ArrayNumeros.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class ArrayNumeros {
     public static void main(String[] args) {
         int[] numeros = new int[500];
+        Random r = new Random();
         for (int i = 0; i < numeros.length; i++) {
-            Random r = new Random();
             numeros[i] = r.nextInt(1001);
         }
         for (int i : numeros) {
@@ -15,5 +15,11 @@ public class ArrayNumeros {
                 System.out.println(i);
             }
         }
+        // - Usando bucle 'fori' en lugar de 'for each'
+        // . for (int i = 0; i < numeros.length; i++) {
+        // . if(numeros[i]>800 && numeros[i]%2==0){
+        // . System.out.println(numeros);
+        // . }
+        // .}
     }
 }
