70c9e4754d40f0cd51df7f3327a98cc08d828e83
Adrian <adrian.luque.rod@gmail.com>
2024-01-27 20:21:56 +0100

    u04e0X

--- EjerciciosClase/u04/src/main/java/e07_e12/TestDispositivos.java
+++ EjerciciosClase/u04/src/main/java/e07_e12/TestDispositivos.java
@@ -2,12 +2,18 @@ package e07_e12;
 
 public class TestDispositivos {
     public static void main(String[] args) {
+        final int NUMVECES = 100;
         Procesador p1 = new Procesador("pmarc1", "pmod1");
         Procesador p2 = new Procesador("pmarc2", "pmod2");
         MemoriaRAM m1 = new MemoriaRAM("mMar1", "mMod1");
         MemoriaRAM m2 = new MemoriaRAM("mMar2", "mMod2");
         TarjetaVideo t1 = new TarjetaVideo("tmarc1", "tmod1");
         Dispositivo listaDispositivos[] = { p1, p2, m1, m2, t1 };
-
+        for (Dispositivo a : listaDispositivos) {
+            for (int i = 0; i < NUMVECES; i++) {
+                a.encender();
+                a.apagar();
+            }
+        }
     }
 }
