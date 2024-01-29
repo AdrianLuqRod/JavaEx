72b1415046c3b1aafc89701cc898dc1ce680ec75
Adrian <adrian.luque.rod@gmail.com>
2024-01-29 13:24:37 +0100

    df

--- EjerciciosClase/u04/src/main/java/exE02/CajaCarton.java
+++ EjerciciosClase/u04/src/main/java/exE02/CajaCarton.java
@@ -1,12 +1,19 @@
 package exE02;
 
 public class CajaCarton implements Almacenable {
+    public final int TAM = 100;
+    private int estanteria[] = new int[TAM];
+    private int codigo;
     private double anchura, altura, profundidad;
 
     public CajaCarton(double anchura, double altura, double profundidad) {
         this.anchura = anchura;
         this.altura = altura;
         this.profundidad = profundidad;
+    }
+
+    public int getCodigo() {
+        return codigo;
     }
 
     public double calculaVolumen() {
@@ -27,5 +34,13 @@ public class CajaCarton implements Almacenable {
 
     public void almacena(int numEstanteria) {
 
+        System.out.println("Se ha almacenado la caja con codigo " + codigo + " en la estanteria " + numEstanteria);
+    }
+
+    public int localiza() {
+        return 1;
+    }
+
+    public void retira() {
     }
 }
--- EjerciciosClase/u04/src/main/java/exE02/PruebaAlmacenable.java
+++ EjerciciosClase/u04/src/main/java/exE02/PruebaAlmacenable.java
@@ -4,8 +4,17 @@ import java.util.Scanner;
 
 public class PruebaAlmacenable {
     public static void main(String[] args) {
+        int opcion;
         Scanner sc = new Scanner(System.in);
+        System.out.printf("Elige una opción: ");
         do {
-        } while (opcion != 0);
+            System.out.println("\n0- Salir\n1-Almacena");
+            opcion = sc.nextInt();
+            sc.nextLine();
+            if (opcion != 0 && opcion != 1) {
+                System.out.printf("Opcion incorrecta, elige de nuevo: ");
+            }
+        } while (opcion != 0 && opcion != 1);
+
     }
 }
