679030a3c7a2330f96b2b9799fab14bb71c2eb12
Adrian <adrian.luque.rod@gmail.com>
2024-01-29 12:53:06 +0100

    u04Ex02

--- EjerciciosClase/u04/src/main/java/exE02/CajaCarton.java
+++ EjerciciosClase/u04/src/main/java/exE02/CajaCarton.java
@@ -1,5 +1,31 @@
 package exE02;
 
 public class CajaCarton implements Almacenable {
+    private double anchura, altura, profundidad;
 
+    public CajaCarton(double anchura, double altura, double profundidad) {
+        this.anchura = anchura;
+        this.altura = altura;
+        this.profundidad = profundidad;
+    }
+
+    public double calculaVolumen() {
+        return anchura * profundidad;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double getAnchura() {
+        return anchura;
+    }
+
+    public double getProfundidad() {
+        return profundidad;
+    }
+
+    public void almacena(int numEstanteria) {
+
+    }
 }
---
+++ EjerciciosClase/u04/src/main/java/exE02/PruebaAlmacenable.java
@@ -0,0 +1,11 @@
+package exE02;
+
+import java.util.Scanner;
+
+public class PruebaAlmacenable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        do {
+        } while (opcion != 0);
+    }
+}
