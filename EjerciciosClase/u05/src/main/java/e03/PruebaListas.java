0d502be23a6bf1619ad3e9bf6a96304d68d77590
Adrian <adrian.luque.rod@gmail.com>
2024-02-04 20:15:59 +0100

    u05

--- EjerciciosClase/u05/src/main/java/e03/PruebaListas.java
+++ EjerciciosClase/u05/src/main/java/e03/PruebaListas.java
@@ -1,5 +1,45 @@
 package e03;
 
-public class PruebaListas {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
+public class PruebaListas {
+    public static void main(String[] args) {
+        List meses = new ArrayList();
+        meses.add("Enero");
+        meses.add("Febrero");
+        meses.add("Marzo");
+        meses.add("Abril");
+        meses.add("Mayo");
+        meses.add("Junio");
+        meses.add("Julio");
+        meses.add("Agosto");
+        meses.add("Septiembre");
+        meses.add("Octubre");
+        meses.add("Noviembre");
+        meses.add("Diciembre");
+        System.out.println("Esta la lista vacia?: " + meses.isEmpty());
+        System.out.println("Cuantos elementos tiene?: " + meses.size());
+        System.out.println("Contiene el mes 'Henero'?: " + meses.contains("Henero"));
+
+        Iterator iter = meses.iterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next());
+
+        }
+        System.out.println("");
+        Collections.sort(meses);
+        iter = meses.iterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next());
+        }
+        System.out.println("");
+        meses.clear();
+        iter = meses.iterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next());
+        }
+    }
 }
